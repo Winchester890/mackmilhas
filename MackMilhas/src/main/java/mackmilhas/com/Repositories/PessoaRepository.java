2958bf66d63afceac3664e42cb22dfaf626f3417
Winchester890 <126791337+Winchester890@users.noreply.github.com>
2024-05-16 00:01:40 -0300

    Update PessoaRepository.java

--- MackMilhas/src/main/java/mackmilhas/com/Repositories/PessoaRepository.java
+++ MackMilhas/src/main/java/mackmilhas/com/Repositories/PessoaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<PessoaEntity, Long> {
-
+    PessoaEntity findBySenha(String senha);
 }
