885a72323802e13efcbaad43869b212e93236085
Winchester890 <126791337+Winchester890@users.noreply.github.com>
2024-05-17 19:05:37 -0300

    Update ReservaRepository.java

--- MackMilhas/src/main/java/mackmilhas/com/Repositories/ReservaRepository.java
+++ MackMilhas/src/main/java/mackmilhas/com/Repositories/ReservaRepository.java
@@ -4,7 +4,9 @@ import mackmilhas.com.Entities.ReservaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservaRepository extends JpaRepository<ReservaEntity, Long> {
-
+    List<ReservaEntity> findByIdPessoa(Long idPessoa);
 }
